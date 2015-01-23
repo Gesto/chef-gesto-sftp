68bc160822855882a7530ccbeef8768014ac8151
Jason Pringle <jason@gestotech.com>
2015-01-23 01:32:07 -0800

    change directory for data feeds

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,11 @@
 #
 # Cookbook Name: gesto-sftp
 #
+
+default['openssh']['server']['match'] = {
+  'Group sftp' => {
+    'ChrootDirectory' => '/mnt/data-feeds/%u',
+    'ForceCommand' => 'internal-sftp',
+    'AllowTcpForwarding' => 'no'
+  }
+}
